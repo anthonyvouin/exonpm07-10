abf58f304600e135d3d01a46352b83184cd5816f
anthonyvouin <72597812+anthonyvouin@users.noreply.github.com>
2021-10-07 14:25:38 +0100

    Update query.js

--- src/query.js
+++ src/query.js
@@ -7,3 +7,21 @@ export default async function retrieveContent() {
   }
   
   
+export default async function buffalo() {
+    const url2 = "https://api.gill-cote-bistro.fr/specials?_sort=id:asc";
+    const reponse2 = await fetch(url);
+    return reponse2.json();  
+  }
+  
+  export default async function buffalo3() {
+    const url3 = "https://api.gill-cote-bistro.fr/suggestions?_sort=id:asc)";
+    const reponse3 = await fetch(url);
+    return reponse3.json();  
+  }
+  
+  export default async function buffalo4() {
+    const url4 = "https://api.gill-cote-bistro.fr/histoires?_sort=id:asc";
+    const reponse4 = await fetch(url);
+    return reponse4.json();  
+  }
+  
