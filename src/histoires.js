87873172d4c694e26c26b92243cb131be3943b43
anthonyvouin <72597812+anthonyvouin@users.noreply.github.com>
2021-10-07 14:40:59 +0100

    Update histoires.js

--- src/histoires.js
+++ src/histoires.js
@@ -1,6 +1,6 @@
 
   
-  export default async function buffaloHistoires() {
+    export default async function buffaloHistoires() {
     const url = "https://api.gill-cote-bistro.fr/histoires?_sort=id:asc";
     const reponse = await fetch(url);
     return reponse.json();  
