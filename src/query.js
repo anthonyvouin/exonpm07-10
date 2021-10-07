7104df1500981317a26f4c0fab4f04344d803304
anthonyvouin <72597812+anthonyvouin@users.noreply.github.com>
2021-10-07 14:24:15 +0100

    Update query.js

--- src/query.js
+++ src/query.js
@@ -13,13 +13,13 @@ export default async function buffalo() {
     return reponse2.json();  
   }
   
-  export default async function buffalo() {
+  export default async function buffalo3() {
     const url3 = "https://api.gill-cote-bistro.fr/suggestions?_sort=id:asc)";
     const reponse3 = await fetch(url);
     return reponse3.json();  
   }
   
-  export default async function buffalo() {
+  export default async function buffalo4() {
     const url4 = "https://api.gill-cote-bistro.fr/histoires?_sort=id:asc";
     const reponse4 = await fetch(url);
     return reponse4.json();  
