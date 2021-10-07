1a70277cef36587f3ef266b9b06e6a6239845e2c
anthonyvouin <72597812+anthonyvouin@users.noreply.github.com>
2021-10-07 16:08:06 +0100

    c'est carré
    Jai tout compris chef

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import buffaloHistoires from './histoires.js';
 
 async function showContent() {
   const content = await buffaloSpecial();
+  
   const url = "https://api.gill-cote-bistro.fr"
   try {
     
@@ -16,11 +17,14 @@ async function showContent() {
       let image = document.createElement('img');
       image.setAttribute('src',url + content[i].image.url)
   
+      let description = document.createElement('p');
+      description.textContent = content[i].description;
+
       document.getElementsByTagName('body')[0].appendChild(elt);
       document.getElementsByTagName('body')[0].appendChild(image);
-    }
+      document.getElementsByTagName('body')[0].appendChild(description);
 
-   
+    }  
     
   } catch (e) {
     console.log('Error', e);
@@ -28,3 +32,35 @@ async function showContent() {
 }
 
 showContent();
+
+
+
+async function showContent2() {
+  const content2 = await buffaloSuggestions();
+  
+  const url = "https://api.gill-cote-bistro.fr"
+  try {
+    
+    for(let i=0; i<content2.length ; i++) {
+
+      let elt = document.createElement('p');
+      elt.textContent = content2[i].suggestionsStarter;    
+
+
+      let price = document.createElement('p');
+      price.textContent = content2[i].suggestionsStarterPrice;    
+
+
+      document.getElementsByTagName('body')[0].appendChild(elt);
+      document.getElementsByTagName('body')[0].appendChild(price);
+
+      
+      
+    }  
+    
+  } catch (e) {
+    console.log('Error', e);
+  }
+}
+
+showContent2();
--- src/suggestions.js
+++ src/suggestions.js
@@ -1,5 +1,5 @@
 export default async function buffaloSuggestions() {
-    const url = "https://api.gill-cote-bistro.fr/suggestions?_sort=id:asc)";
+    const url = "https://api.gill-cote-bistro.fr/suggestions?_sort=id:asc";
     const reponse = await fetch(url);
     return reponse.json();  
   }
